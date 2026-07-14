7bc75ac5f5b21242993312276d10c0849206c3d7
red_concrete <leonard@ozander.de>
2026-07-14 18:42:16 +0200

    Integrate Hilt for dependency injection; update AndroidManifest.xml and Gradle files for configuration

---
+++ app/src/main/java/kittycards/kittycardsandroid/KittyCardsApplication.java
@@ -0,0 +1,8 @@
+package kittycards.kittycardsandroid;
+
+import android.app.Application;
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class KittyCardsApplication extends Application {
+}
