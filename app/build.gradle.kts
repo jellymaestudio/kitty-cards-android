8a8d148deddd9791b259e51e2305398e26702490
red_concrete <leonard@ozander.de>
2026-07-14 18:42:16 +0200

    Integrate Hilt for dependency injection; update AndroidManifest.xml and Gradle files for configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.compose.compiler)
     alias(libs.plugins.android.junit)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -19,7 +21,7 @@ android {
         versionCode = 1
         versionName = "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunner = "kittycards.kittycardsandroid.HiltTestRunner"
     }
 
     buildTypes {
@@ -41,31 +43,40 @@ android {
 }
 
 dependencies {
+    // UI & Core
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.activity.compose)
-
-    implementation(libs.androidx.activity.ktx)
     implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.activity.ktx)
     implementation(libs.material)
 
-    androidTestImplementation(libs.androidx.espresso.core)
+    // Dependency Injection
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
 
-    testImplementation(platform(libs.junit.bom))        // BOM zieht alle Versionen rein
+    // Tests (Unit)
+    testImplementation(platform(libs.junit.bom))
+    testImplementation(libs.androidx.core)
+    testImplementation(libs.junit)
     testImplementation(libs.junit.jupiter.api)
     testImplementation(libs.junit.jupiter.params)
-    testRuntimeOnly(libs.junit.jupiter.engine)
-
-    testImplementation(libs.junit)                      // JUnit 4 bleibt
-    testRuntimeOnly(libs.junit.vintage.engine)
-
-    androidTestImplementation(libs.androidx.junit)
-
-    testImplementation(libs.robolectric)
     testImplementation(libs.mockito.core)
     testImplementation(libs.mockito.junit.jupiter)
-    testImplementation(libs.androidx.core)
+    testImplementation(libs.robolectric)
+    testRuntimeOnly(libs.junit.jupiter.engine)
+    testRuntimeOnly(libs.junit.vintage.engine)
+
+    // Tests (Instrumentation)
+    androidTestImplementation("androidx.test:rules:1.7.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
+    androidTestImplementation("androidx.test.espresso:espresso-idling-resource:3.7.0")
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
+    androidTestImplementation(libs.hilt.testing)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.androidx.junit)
+    kspAndroidTest(libs.hilt.compiler)
 }
